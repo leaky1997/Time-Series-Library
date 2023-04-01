bdcf1913d9aaad73b5352814905dbb10fb7b7b58
Liqi <18896782803@163.com>
2023-04-01 10:32:17 +0800

    +1

---
+++ scripts/long_term_forecast/ECL_script/TSMixer.sh
@@ -0,0 +1,162 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=TSMixer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 512 \
+  --label_len 48 \
+  --pred_len 96 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --depth 1 \
+  --L1 8 \
+  --L2 8 \
+  --L3 8 \
+  --kernel_size 3 \
+  --expansion_factor 1\
+  --des 'Exp' \
+  --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_96 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --depth 1 \
+#   --L1 6 \
+#   --L2 4 \
+#   --L3 4 \
+#   --kernel_size 3 \
+#   --expansion_factor 8\
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_96 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --depth 1 \
+#   --L1 6 \
+#   --L2 4 \
+#   --L3 4 \
+#   --kernel_size 3 \
+#   --expansion_factor 8\
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_96 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --depth 1 \
+#   --L1 6 \
+#   --L2 4 \
+#   --L3 4 \
+#   --kernel_size 3 \
+#   --expansion_factor 8\
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_192 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_336 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --des 'Exp' \
+#   --itr 1
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/electricity/ \
+#   --data_path electricity.csv \
+#   --model_id ECL_96_720 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --des 'Exp' \
+#   --itr 1
